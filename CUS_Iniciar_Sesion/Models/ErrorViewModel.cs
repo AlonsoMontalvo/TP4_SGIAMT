3ae7f938f8c9b2e6c8969109f570b5dd26b621af
Alumno <alumno@laboratorios.urp.edu.pe>
2019-11-14 20:03:15 -0500

    avanze login

---
+++ CUS_Iniciar_Sesion/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CUS_Iniciar_Sesion.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
